0015e2c52c9e7fa7914fff1ad0650ca60642fd80
Cristian <cgiambruni@gmail.com>
2014-09-11 19:31:22 -0300

    Ejercicio3 - Practica2

--- practica2/ejercicio1/contador.php
+++ practica2/ejercicio1/contador.php
@@ -6,6 +6,8 @@
 <body>
 	<h1> En este ejemplo leeremos el contenido del archivo contador.txt, lo mostramos al usuario y lo incrementaremos en 1</h1>
 	<h2> Tener en cuenta los permisos del archivo.... </h2>
+
+
 <?php
 $archivo = "contador.txt";
 $contador = 0;
@@ -16,11 +18,18 @@ fclose($fp);
 
 ++$contador;
 
+
+
+
+
+
 $fp = fopen($archivo,"w+");
 fwrite($fp, $contador);
 fclose($fp);
 
 echo "Esta página ha sido visitada $contador veces";
 ?> 
+
+
 </body>
 </html>
