2d39b7fce9e4e63b48d3bc80552933630986acbc
Cristian <cgiambruni@gmail.com>
2014-09-28 17:48:41 -0300

    Maldito git que no toma la última modificación mía

--- entrega/backend.php
+++ entrega/backend.php
@@ -6,6 +6,7 @@ session_start();
 //validamos si se ha hecho o no el inicio de sesion correctamente
 
 //si no se ha hecho la sesion nos regresará a login.php
+
 if(!isset($_SESSION['usuario'])) 
 {
   header('Location: index.html'); 
@@ -62,7 +63,7 @@ if(!isset($_SESSION['usuario']))
 				
             </div>
 			
-            <!-- Pie de página -->
+            <!-- Pie de página con css -->
             <div id ="pie">
 			
 			<script> type="text/javascript">
