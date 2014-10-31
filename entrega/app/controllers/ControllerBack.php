291def133015281e1d1e936be89feb6c821ab0e8
Cristian <cgiambruni@gmail.com>
2014-10-30 21:36:13 -0300

    Mensaje de OK cuando se da de alta un usuario

--- entrega/app/controllers/ControllerBack.php
+++ entrega/app/controllers/ControllerBack.php
@@ -357,7 +357,10 @@ require_once __DIR__ . '/ControllerLogin.php';
 			// comprueba que el usuario no exista ya
 			if (!$this->us->existeUsuario($_POST['nombre'])){
 				 $this->us->agregar($_POST['nombre'], $_POST['rol'], $_POST['p1']);
-				 header('Location: backend.php?accion=users');
+				 /*header('Location: backend.php?accion=users');*/
+				 $params = array('users' => $this->us->listar());
+				 echo $this->twig->render('abmUsers.html', array('users' => $params['users'], 'mensaje' => 'Se ha agregado bien'));
+				 /* 'usuario' => dameUsuario())*/
 			}
             else { // mostrar mensaje, lo hiciste mal, llenalo de nuevo
                  header('Location: backend.php?accion=users#err2');
